e84b8c544b2fc140995c534f9cd505cef6d13f26
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2019-07-13 21:15:57 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include "Game"
+
+using namespace std;
+
+
+int main()
+{
+	int option, choice;
+	Game g;
+	cout << "\nEnter 1 To Play\n";
+	cin >> option;
+	while (option == 1)
+	{
+		g.player_details();
+		do {
+			int i;
+			g.check_wager();
+			cout<<"\nEnter Your Guess\n";
+			cin>>i;
+			g(i);
+			cout << "\nEnter 1 to continue\n";
+		} while (choice == 1);
+	}
+
+	return 0;
+}
