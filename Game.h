5e43440e331a5f7d4479e7313d161f419ed5e3be
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2019-07-18 18:29:26 +0530

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,23 +1,22 @@
-#ifndef Game
-#def Game
-
+#ifndef GAME_H
+#define GAME_H
 class Game
 {
-	string name;
-	string rule1 = "Your Wager Must Not Exceed  Your Deposit\n";
-	string rule2 = "The Guess Must Lie Between 0 and 10";
-	string rule3 = "For Every Win You Are Returned 10 times the wager\nFor Every Loss, You Return You Wager";
-	string win = "YOU WIN!";
-	string loss = "SORRY FOR YOUR LOSS!";
+	std::string name;
+	std::string rule1 = "Your Wager Must Not Exceed  Your Deposit\n";
+	std::string rule2 = "The Guess Must Lie Between 0 and 10";
+	std::string rule3 = "For Every Win You Are Returned 10 times the wager\nFor Every Loss, You Return You Wager";
+	std::string win = "YOU WIN!";
+	std::string loss = "SORRY FOR YOUR LOSS!";
 	int deposit, wager, dice, guess;
-	void Game::check_value();
 
 public:
 	void player_details();
 	void check_wager();
+	void check_value();
 	Game();
 	Game(int x);
-	
+
 };
 
 #endif
